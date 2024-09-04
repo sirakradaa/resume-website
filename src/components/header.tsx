4eb021578c9840244f51c7cb388d2c31a4c765a8
Sirak Radaa <60719924+sirakradaa@users.noreply.github.com>
2024-09-03 21:59:34 -0600

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,25 @@ import {
   Button,
   useColorMode,
   useColorModeValue,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useBreakpointValue,
 } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { NavLink, useLocation } from "react-router-dom";
+import {
+  MoonIcon,
+  SunIcon,
+  DownloadIcon,
+  HamburgerIcon,
+} from "@chakra-ui/icons";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
 
   const bgColor = useColorModeValue(
     isScrolled ? "rgba(194, 87, 87, 0.8)" : "transparent",
@@ -20,6 +32,8 @@ const Header = () => {
   const textColor = useColorModeValue("brand.cream", "brand.cream");
   const boxShadow = isScrolled ? "0 4px 6px rgba(0, 0, 0, 0.1)" : "none";
   const backdropFilter = isScrolled ? "blur(5px)" : "none";
+
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   useEffect(() => {
     const handleScroll = () => {
@@ -36,6 +50,81 @@ const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const handleResumeDownload = () => {
+    // Replace 'path_to_your_resume.pdf' with the actual path to your resume file
+    const resumeUrl =
+      "https://docs.google.com/document/d/1pOMsrRROz-R5PxtvJ_3UZOGst2ImUohX/export?format=pdf";
+    const link = document.createElement("a");
+    link.href = resumeUrl;
+    link.download = "Your_Name_Resume.pdf"; // Replace with your name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const hoverStyle = {
+    bg: colorMode === "light" ? "brand.beaver" : "brand.yinMnBlue",
+    color: "brand.cream",
+  };
+
+  const menuItems = ["Work", "About", "Projects", "Skills", "Contact"];
+
+  const MenuItemLink = ({
+    to,
+    children,
+  }: {
+    to: string;
+    children: React.ReactNode;
+  }) => {
+    const isActive =
+      location.pathname === to || (to === "/" && location.pathname === "/work");
+    const activeStyle = {
+      fontWeight: "bold",
+      backgroundColor:
+        colorMode === "light" ? "brand.beaver" : "brand.yinMnBlue",
+      color: "brand.cream",
+    };
+    const inactiveStyle = {
+      fontWeight: "normal",
+      backgroundColor: "transparent",
+      color: textColor,
+    };
+
+    return (
+      <NavLink to={to} style={isActive ? activeStyle : inactiveStyle}>
+        <Box width="100%" display="block" padding="0.5rem 1rem">
+          {children}
+        </Box>
+      </NavLink>
+    );
+  };
+
+  const NavItems = () => (
+    <>
+      {menuItems.map((item) => (
+        <NavLink
+          key={item}
+          to={item === "Work" ? "/" : `/${item.toLowerCase()}`}
+          style={({ isActive }) => ({
+            fontWeight: isActive ? "bold" : "normal",
+            marginRight: "1rem",
+          })}
+        >
+          <Box
+            as="span"
+            px={2}
+            py={1}
+            borderRadius="md"
+            _hover={hoverStyle}
+            transition="all 0.3s"
+          >
+            {item}
+          </Box>
+        </NavLink>
+      ))}
+    </>
+  );
 
   return (
     <Box
@@ -55,62 +144,82 @@ const Header = () => {
       backdropFilter={backdropFilter}
       transition="all 0.3s ease-in-out"
     >
-      <Flex justify="center" align="center">
-        <Flex justify="space-between" align="center" width="100%" maxW="800px">
-          <NavLink
-            to="/"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-              marginRight: "1rem",
-            })}
-          >
-            Work
-          </NavLink>
-          <NavLink
-            to="/about"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-              marginRight: "1rem",
-            })}
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/projects"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-              marginRight: "1rem",
-            })}
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/skills"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-              marginRight: "1rem",
-            })}
-          >
-            Skills
-          </NavLink>
-          <NavLink
-            to="/contact"
-            style={({ isActive }) => ({
-              fontWeight: isActive ? "bold" : "normal",
-              marginRight: "1rem",
-            })}
-          >
-            Contact
-          </NavLink>
-          <Button
-            color={textColor}
-            onClick={toggleColorMode}
-            size="sm"
-            variant="ghost"
-          >
-            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-          </Button>
-        </Flex>
+      <Flex justify="space-between" align="center">
+        {isMobile ? (
+          <>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+                size="lg"
+              />
+              <MenuList>
+                {menuItems.map((item) => (
+                  <MenuItem key={item} as="div" p={0}>
+                    <MenuItemLink
+                      to={item === "Work" ? "/" : `/${item.toLowerCase()}`}
+                    >
+                      {item}
+                    </MenuItemLink>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+            <Flex>
+              <Button
+                leftIcon={<DownloadIcon />}
+                onClick={handleResumeDownload}
+                size="sm"
+                variant="ghost"
+                color={textColor}
+                mr={2}
+                _hover={hoverStyle}
+              >
+                Resume
+              </Button>
+              <Button
+                color={textColor}
+                onClick={toggleColorMode}
+                size="sm"
+                variant="ghost"
+                _hover={hoverStyle}
+              >
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Flex>
+          </>
+        ) : (
+          <Flex width="100%" justify="space-between" align="center">
+            <Box flex="1" /> {/* This creates space on the left */}
+            <Flex align="center" justify="center" flex="2">
+              <NavItems />
+            </Flex>
+            <Flex align="center" justify="flex-end" flex="1">
+              <Button
+                leftIcon={<DownloadIcon />}
+                onClick={handleResumeDownload}
+                size="sm"
+                variant="ghost"
+                color={textColor}
+                mr={2}
+                _hover={hoverStyle}
+              >
+                My Resume
+              </Button>
+              <Button
+                color={textColor}
+                onClick={toggleColorMode}
+                size="sm"
+                variant="ghost"
+                _hover={hoverStyle}
+              >
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Flex>
+          </Flex>
+        )}
       </Flex>
     </Box>
   );
